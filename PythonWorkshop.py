b00076ac12cd68d23bf3f5ac0532d2d193020c26
paul <piankin250@gmail.com>
2020-10-31 17:04:11 +0300

    lesson

---
+++ PythonWorkshop.py
@@ -0,0 +1,23 @@
+import math
+
+#math
+print("math:")
+print(2 + 2)
+print(5 - 6)
+print(6 / 2) #always float
+print(2.0 + 1)
+print(2*3)
+print(2**3)
+print(3%2)
+print(4%2)
+print(1.2*3)
+print(round(1.2*3, 2))
+print(math.sqrt(4))
+
+#variables
+print("variable:")
+x = 1
+print(x)
+x = x+2
+print(x)
+print(type(x))
