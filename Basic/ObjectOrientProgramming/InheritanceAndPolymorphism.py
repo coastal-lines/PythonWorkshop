ff6f8d510384e4f8922fe3c3889f47db955020d1
paul <piankin250@gmail.com>
2020-11-12 23:53:16 +0300

    OOP

--- Basic/ObjectOrientProgramming/InheritanceAndPolymorphism.py
+++ Basic/ObjectOrientProgramming/InheritanceAndPolymorphism.py
@@ -13,3 +13,22 @@ class Shape():
         print('calc perimeter')
 
 shape = Shape()
+
+class Rectangle(Shape):
+    
+    def __init__(self, width, height):
+        Shape.__init__(self)
+        
+        self.width = width
+        self.height = height
+        
+        print('Rectangle created')
+        
+    def draw(self):
+        print(f'Drawing rectangle with width={self.width} and height={self.height}')
+        
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2*(self.width + self.height)
