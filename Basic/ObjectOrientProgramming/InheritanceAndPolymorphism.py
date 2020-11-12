ef4dade0c5a5967e83a9ad1f866bcf5710a2d72e
paul <piankin250@gmail.com>
2020-11-12 21:52:59 +0300

    OOP

---
+++ Basic/ObjectOrientProgramming/InheritanceAndPolymorphism.py
@@ -0,0 +1,15 @@
+class Shape():
+    
+    def __init__(self):
+        print('Shape created')
+        
+    def draw(self):
+        print('Drawing a shape')
+    
+    def area(self):
+        print('calc area')
+        
+    def perimeter(self):
+        print('calc perimeter')
+
+shape = Shape()
