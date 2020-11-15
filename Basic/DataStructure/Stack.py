7e38e322a666807110484a98a5b8297ed3e7b9ce
paul <piankin250@gmail.com>
2020-11-16 00:21:16 +0300

    Stack

---
+++ Basic/DataStructure/Stack.py
@@ -0,0 +1,30 @@
+class MyStack:
+    def __init__(self):
+        self.array = []
+
+    def push(self, item):
+        self.array.append(item)
+
+    def pop(self):
+        popped_item = self.array.pop()
+        return popped_item
+
+    def peek(self):
+        return self.__current()
+
+    def __current(self):
+        self.array[self.count() - 1]
+
+    def count(self):
+        return len(self.array)
+
+    def __iter__(self):
+        self.index = self.count() - 1
+        return self
+
+    def __next__(self):
+        if self.index < 0:
+            raise StopIteration()
+        result = self.array[self.index]
+        self.index -= 1
+        return result
