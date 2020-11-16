1d7a57e314f0a459897ae50b58592250fd3da99c
paul <piankin250@gmail.com>
2020-11-16 21:54:19 +0300

    Singletone

---
+++ Basic/Singleton.py
@@ -0,0 +1,10 @@
+class Character:
+    _instanse = None
+    def __new__(cls):
+        if not cls._instanse:
+            cls._instanse = super().__new__(cls)
+        return cls._instanse
+    
+    def __init__(self):
+        self.race = 'Elf'
+
