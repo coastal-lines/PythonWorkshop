d6934aa28806fe56d34148fd3a766276fccd9118
paul <piankin250@gmail.com>
2020-12-03 01:21:50 +0300

    Optimize

--- Math/Derivative.py
+++ Math/Derivative.py
@@ -5,8 +5,19 @@ from sympy import *
 #result = diff(x**2 + y**2, x)
 #print(result)
 
+'''
 x, y, z = symbols('x y z')
 f = 2*x**2 - 4*x*z + 4*y**2 - 8*y*z + 9*z**2 + 4*x + 8*y - 20*z
+resultX = diff(f, x)
+resultY = diff(f, y)
+resultZ = diff(f, z)
+print(resultX)
+print(resultY)
+print(resultZ)
+'''
+
+x, y, z = symbols('x y z')
+f = 2*x**2 - 4*x*z + 4*y**2 - 8*y*z + 9*z**2 +4*x + 8*y - 20*z
 resultX = diff(f, x)
 resultY = diff(f, y)
 resultZ = diff(f, z)
