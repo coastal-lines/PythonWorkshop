ece05852638d4cf62d3e0444c38b460980aec2bc
paul <piankin250@gmail.com>
2020-12-16 00:00:33 +0300

    ...

--- OpenCV/Contours.py
+++ OpenCV/Contours.py
@@ -1,5 +1,16 @@
-from __future__ import print_function
-import cv2 as cv
+import cv2
 import numpy as np
-import argparse
-import random as rng
+import matplotlib.pyplot as plt
+
+#open image, conver into RGB, convert into BW
+image_original = cv2.imread(r'C:\Temp2\ForTesseract\labeled\pic.png', cv2.IMREAD_GRAYSCALE)
+img_bw_rgb = cv2.cvtColor(image_original, cv2.COLOR_BGR2RGB)
+
+median_intensity = np.median(img_bw_rgb)
+lower_threshold = int(max(0, (1.0 - 0.33) * median_intensity))
+upper_threshold = int(min(255, (1.0 + 0.33) * median_intensity))
+img_canny = cv2.Canny(img_bw_rgb, lower_threshold, upper_threshold)
+
+#open image in popup
+imgplot = plt.imshow(img_canny[0:100])
+plt.show()
