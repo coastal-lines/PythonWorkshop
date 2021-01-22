0a5fa29000109884d6c3f2da365f72a64933257b
paul <piankin250@gmail.com>
2021-01-22 23:10:43 +0300

    gui

--- DataVisualization/FindCasesInFolder.py
+++ DataVisualization/FindCasesInFolder.py
@@ -1,5 +1,6 @@
 import requests
 from pyral import Rally, rallyWorkset
+import matplotlib.pyplot as plt
 
 
 
@@ -28,11 +29,36 @@ def login(folderID):
     rally.setWorkspace(rootWorkspace.Name)
     rally.setProject(rootProject.Name)
 
+    folder_id = "TF22587"
+
     query_criteria = 'FormattedID = "TF22587"' # + folderID
     #testFolder = rally.get('TestFolder',  query=query_criteria, fetch=True, instance=True)
-    testFolder = rally.get('TestFolder', fetch='ValidationInput', query=query_criteria, instance=True)
-    testFolderChildrens = testFolder.Children
-    a = 100
+    #testFolder = rally.get('TestFolder', fetch='True', query=query_criteria, instance=True)
+    #folders = rally.get('TestFolder', query='TestFolder.Name = TF22587', instance=True)
+    #testFolderChildrens = testFolder.Children
+    query = 'FormattedID = %s'
+    test_folder_req = rally.get('TestFolder', fetch=True, projectScopeDown=True, query=query % 'TF22587') # folder_id == "TF111"
+    test_folder = test_folder_req.next()
+    test_cases = test_folder.TestCases
+    print('Start working with %s' % folder_id)
+    print('Test Folder %s contains %s TestCases' % (folder_id, len(test_cases)))
+    list_steps = None
+    for test_case in test_cases:
+        print(test_case.FormattedID)
+        print(test_case.Name)
+        list_steps=test_case.Steps
+        listInputs = []
+        listExpectedResult = []
+        for i in list_steps:
+            listInputs.append(i.Input)
+            print(i.Input)
+        for er in list_steps:
+            listExpectedResult.append(er.ExpectedResult)
+            print(er.ExpectedResult)
+    index = ["0","1","2","3","4"]
+    values = [5,7,3,4,6]
+    plt.bar(index,values)
+    plt.show()
 
 login("TF22587")
 a = 100
