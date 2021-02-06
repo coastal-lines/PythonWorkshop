5423358a1d69ad80f9a7dc60f33ab173acfbcd3c
paul <piankin250@gmail.com>
2021-02-06 23:47:12 +0300

    guiRally

---
+++ DataVisualization/Visualization/BarClass.py
@@ -0,0 +1,6 @@
+class BarClass():
+    def __init__(self, x, countManualTestCases, countAutomatedTestCases, headFolderdName):
+        self.x = x
+        self.countManualTestCases = countManualTestCases
+        self.countAutomatedTestCases = countAutomatedTestCases
+        self.headFolderdName = headFolderdName
