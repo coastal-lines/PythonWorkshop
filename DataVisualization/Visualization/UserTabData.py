df6928b82af2cb1d5cc327e8533f44b2d9e0c132
paul <piankin250@gmail.com>
2021-02-18 19:12:29 +0300

    RallyDraft

---
+++ DataVisualization/Visualization/UserTabData.py
@@ -0,0 +1,6 @@
+class UserTabData():
+    def __init__(self, tabIndex, tidyData, untidyData, rootFolderName):
+        self.tabIndex = tabIndex
+        self.tidyData = tidyData
+        self.untidyData = untidyData
+        self.rootFolderName = rootFolderName
