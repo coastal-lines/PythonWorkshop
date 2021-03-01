f8570814bdbfaeaa574f45bfaa79b2e4838a0b58
paul <piankin250@gmail.com>
2021-03-01 21:34:16 +0300

    opencv

--- OpenCV/School/module1.py
+++ OpenCV/School/module1.py
@@ -7,6 +7,28 @@ filterTests = None
 #open image
 img = cv.imread(r'C:\Temp2\ForTesseract\pic.png')
 
+def findTrianglesByColor(roi, min, max, wUser, hUser):
+    #min color
+    min = np.array(min)
+    #max color
+    max = np.array(max)
+    
+    #remove some details by gaussian blur
+    blur = cv.GaussianBlur(roi, (7, 7), 0)
+    #remove other colors - min color and max color range
+    thresh = cv.inRange(blur, min, max)
+
+    #find contours
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    #find specific triangle by parameters
+    for cnt in contours:
+        x,y,w,h = cv.boundingRect(cnt)
+        if w > wUser and h > hUser:
+            print(x,y,w,h)
+            cv.drawContours(img, [cnt], 0, (0,255,0), 3) # рисуем прямоугольник
+            return x,y,w,h
+
 #find specific triangle coordinates by color segmentation
 def findTriangleByColor(roi, min, max, wUser, hUser):
     #min color
@@ -46,8 +68,11 @@ def findFilterTests():
 
 def findTestNameField():
     #find FilterTests region
-    x,y,w,h = findTriangleByColor(filterTests, [245, 245, 245], [255, 255, 255], 209, 550)
-    filterTests = cutArea(img, x, y, w, h)
+    w = img.shape[1]
+    h = img.shape[0]
+    roi = img[0:w,0:h]
+    x,y,w,h = findTriangleByColor(roi, [255, 255, 255], [255, 255, 255], 215, 20)
+    testNameField = cutArea(img, x, y, w, h)
 
 findFilterTests()
 findTestNameField()
