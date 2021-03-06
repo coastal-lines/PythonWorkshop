99c66babecefd7e66d076350c2275c4d31b30235
paul <piankin250@gmail.com>
2021-03-06 23:09:20 +0300

    opencv

--- OpenCV/School/TemplateMatching/TM.py
+++ OpenCV/School/TemplateMatching/TM.py
@@ -3,3 +3,10 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import pytesseract
+
+img = cv.imread(r'C:\Temp2\Flash\temp1.bmp')
+templ = cv.imread(r'C:\Temp2\Flash\tmp1.bmp')
+
+
+
+result = cv.matchTemplate(img, templ, cv.TM_CCOEFF)
