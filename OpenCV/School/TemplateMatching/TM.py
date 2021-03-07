0c80baa57db0c730aee4d7e7d710cfe06e3c2a94
paul <piankin250@gmail.com>
2021-03-07 23:42:42 +0300

    opencv

--- OpenCV/School/TemplateMatching/TM.py
+++ OpenCV/School/TemplateMatching/TM.py
@@ -4,9 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pytesseract
 
-img = cv.imread(r'C:\Temp2\Flash\temp1.bmp')
-templ = cv.imread(r'C:\Temp2\Flash\tmp1.bmp')
+img = cv.imread(r'C:\Temp2\Flash\tests4.bmp')
+img_gr = cv.imread(r'C:\Temp2\Flash\tests4.bmp', cv.IMREAD_GRAYSCALE)
+template = cv.imread(r'C:\Temp2\Flash\tmp1.bmp', cv.IMREAD_GRAYSCALE)
+
+w, h = template.shape[::-1]
+
+res = cv.matchTemplate(img_gr, template, cv.TM_CCOEFF_NORMED)
+#norm_results = cv.normalize(res, res, 1, 0, cv.NORM_MINMAX)
+
+threshold = 0.9
+loc = np.where( res >= threshold)
 
 
+for result in loc:
+    #x = round(result[0])
+    #y = round(result[1])
+    x = result[0]
+    y = result[1]
+    point1 = (x , y)
+    point2 = (x + w, y + h)
+    #cv.rectangle(img, point1, point2, (0,255,0), 1)
+    cv.rectangle(img, result, (result[0] + w, result[1] + h), (0,0,255), 1)
+    #cv.rectangle(img, pt,      (pt[0], pt[1]), (0,0,255), 1)
 
-result = cv.matchTemplate(img, templ, cv.TM_CCOEFF)
+#for pt in zip(*loc[::-1]):
+ #   cv.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,255,0), 1)
+
+cv.imshow('Detected', img)
+cv.waitKey(0)
