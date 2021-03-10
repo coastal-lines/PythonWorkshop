cfbf7b4a71fbc17f95e4e1c304b3c7e609402d96
paul <piankin250@gmail.com>
2021-03-10 16:19:39 +0300

    opencv

--- OpenCV/School/TemplateMatching/TM.py
+++ OpenCV/School/TemplateMatching/TM.py
@@ -11,17 +11,21 @@ template = cv.imread(r'C:\Temp2\Flash\tmp1.bmp', cv.IMREAD_GRAYSCALE)
 h, w = template.shape
 
 res = cv.matchTemplate(img_gr, template, cv.TM_SQDIFF)
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
+x = min_loc[0]
+y = min_loc[1]
+point1 = (x , y)
+point2 = (x + w, y + h)
+cv.rectangle(img, point1, point2, (0,255,0), 1)
 
-threshold = 0.9
-loc = np.where( res >= threshold)
+#threshold = 0.9
+#loc = np.where( res >= threshold)
 
-
+"""
 for result in loc:
-    #x = round(result[0])
-    #y = round(result[1])
-    x = result[0]
-    y = result[1]
+    x = min_loc[0]
+    y = min_loc[1]
     point1 = (x , y)
     point2 = (x + w, y + h)
     cv.rectangle(img, point1, point2, (0,255,0), 1)
@@ -30,6 +34,7 @@ for result in loc:
 
 #for pt in zip(*loc[::-1]):
  #   cv.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,255,0), 1)
+"""
 
-cv.imshow('Detected', img)
+cv.imshow('', img)
 cv.waitKey(0)
