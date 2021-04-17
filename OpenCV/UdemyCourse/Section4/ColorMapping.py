925f63fa59c55823e0d08ab67ee1bbcb1cb7a2c4
paul <piankin250@gmail.com>
2021-04-17 17:07:56 +0300

    opencv

--- OpenCV/UdemyCourse/Section4/ColorMapping.py
+++ OpenCV/UdemyCourse/Section4/ColorMapping.py
@@ -2,8 +2,9 @@ import cv2
 import matplotlib.pyplot as plt
 
 img = cv2.imread(r'C:\Users\User\Desktop\Temp\New folder (5)\Clipboard01.jpg')
-
-
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img = cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
+img = cv2.cvtColor(img,cv2.COLOR_RGB2HLS)
 
 plt.imshow(img)
 plt.show()
