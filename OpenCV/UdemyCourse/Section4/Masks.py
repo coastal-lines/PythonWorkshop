13a21cb1359e08f386d5a95ae8ec423990af1a7b
paul <piankin250@gmail.com>
2021-04-20 23:43:32 +0300

    Rallyopencv

--- OpenCV/UdemyCourse/Section4/Masks.py
+++ OpenCV/UdemyCourse/Section4/Masks.py
@@ -1,8 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img1 = cv2.imread(r'C:\Users\User\Desktop\Temp\New folder (5)\Clipboard01.jpg')
-img2 = cv2.imread(r'C:\Users\User\Desktop\Temp\New folder (5)\Clipboard02.jpg')
+img1 = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\dog_backpack.png')
+img2 = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\watermark_no_copy.png')
 img1 = cv2.cvtColor(img1,cv2.COLOR_BGR2RGB)
 img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
 
