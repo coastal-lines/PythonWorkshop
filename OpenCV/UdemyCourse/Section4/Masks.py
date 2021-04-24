b994442b31cc86453abae3e90f09aedbf8fda2c6
paul <piankin250@gmail.com>
2021-04-24 21:17:48 +0300

    opencv

--- OpenCV/UdemyCourse/Section4/Masks.py
+++ OpenCV/UdemyCourse/Section4/Masks.py
@@ -9,19 +9,24 @@ img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
 
 img2 = cv2.resize(img2, (600,600))
 
+#start point for mask
 x_offset = 934 - 600
 y_offset = 1401 - 600
 
-rows,cols,chanels = img2.shape
+#rows,cols,chanels = img2.shape
 
+#Region of Interest (ROI)
 roi = img1[y_offset:1401,x_offset:943]
 
 img2gray = cv2.cvtColor(img2,cv2.COLOR_RGB2GRAY)
 
+#inverse all bits
 mask_inv = cv2.bitwise_not(img2gray)
 
-white_background = np.full(img2.shape, 255,dtype=np.uint8)
+#create white background by numpy
+white_background = np.full(img2.shape,255,dtype=np.uint8)
 
+#Дизьюнкция. Т.е. там где белый, применяем маску
 bk = cv2.bitwise_or(white_background, white_background, mask=mask_inv)
 
 fg = cv2.bitwise_or(img2,img2,mask=mask_inv)
@@ -35,5 +40,5 @@ large_img[y_offset:y_offset+small_img.shape[0], x_offset:x_offset+small_img.shap
 
 
 
-plt.imshow(large_img)
+plt.imshow(img2)
 plt.show()
