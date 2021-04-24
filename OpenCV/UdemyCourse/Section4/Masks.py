81baf6b447221301cdc5c5c0bb66a1e2be86bbd4
paul <piankin250@gmail.com>
2021-04-24 20:40:39 +0300

    opencv

--- OpenCV/UdemyCourse/Section4/Masks.py
+++ OpenCV/UdemyCourse/Section4/Masks.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 img1 = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\dog_backpack.png')
@@ -19,5 +20,20 @@ img2gray = cv2.cvtColor(img2,cv2.COLOR_RGB2GRAY)
 
 mask_inv = cv2.bitwise_not(img2gray)
 
-plt.imshow(mask_inv, cmap='gray')
+white_background = np.full(img2.shape, 255,dtype=np.uint8)
+
+bk = cv2.bitwise_or(white_background, white_background, mask=mask_inv)
+
+fg = cv2.bitwise_or(img2,img2,mask=mask_inv)
+
+final_roi = cv2.bitwise_or(roi,fg)
+
+large_img = img1
+small_img = final_roi
+
+large_img[y_offset:y_offset+small_img.shape[0], x_offset:x_offset+small_img.shape[1]] = small_img
+
+
+
+plt.imshow(large_img)
 plt.show()
