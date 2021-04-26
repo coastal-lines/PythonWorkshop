f1b8f176196f490dd00e8745cf7f182b7e61fb8a
paul <piankin250@gmail.com>
2021-04-26 23:57:43 +0300

    opencv

---
+++ OpenCV/UdemyCourse/Section4/Thresholding.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\rainbow.jpg')
+plt.imshow(img)
+plt.show()
