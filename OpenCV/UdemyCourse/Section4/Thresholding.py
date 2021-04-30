806be3b706a582f946a14bd9e197a8cc4308499a
paul <piankin250@gmail.com>
2021-04-30 23:58:51 +0300

    opencv

--- OpenCV/UdemyCourse/Section4/Thresholding.py
+++ OpenCV/UdemyCourse/Section4/Thresholding.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\rainbow.jpg')
+img = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\rainbow.jpg',0)
 
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+#ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+#ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
 
 plt.imshow(thresh1,cmap='gray')
 plt.show()
