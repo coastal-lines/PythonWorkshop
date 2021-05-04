1efed656665d394734350311e2fea7cdbe7f5846
paul <piankin250@gmail.com>
2021-05-04 23:56:01 +0300

    opencv

--- OpenCV/UdemyCourse/Section4/Thresholding.py
+++ OpenCV/UdemyCourse/Section4/Thresholding.py
@@ -17,7 +17,8 @@ def show_pic(img):
 img = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\crossword.jpg',0)
 ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,8)
-show_pic(th2)
+blended = cv2.addWeighted(src1=th1,alpha=0.6,src2=th2,beta=0.4,gamma=0)
+show_pic(blended)
 
 
 #plt.imshow(img)
