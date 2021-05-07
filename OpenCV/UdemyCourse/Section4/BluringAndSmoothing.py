e9509bfa52691c74293234c4fc421a1543fea2e1
paul <piankin250@gmail.com>
2021-05-07 23:59:29 +0300

    opencv

--- OpenCV/UdemyCourse/Section4/BluringAndSmoothing.py
+++ OpenCV/UdemyCourse/Section4/BluringAndSmoothing.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 def show_pic(img):
     fig = plt.figure(figsize=(15,15))
     ax = fig.add_subplot(111)
-    ax.imshow(img,cmap='gray')
+    ax.imshow(img,cv2.COLOR_BGR2RGB)
     plt.show()
 
-img = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\crossword.jpg',0)
+img = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\bricks.jpg').astype(np.float32) / 255
+
+show_pic(img)
