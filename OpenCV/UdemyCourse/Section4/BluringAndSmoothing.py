fa84da80b41914cc78db63a0e4e112c13a4988f0
paul <piankin250@gmail.com>
2021-05-08 22:45:50 +0300

    opencv

--- OpenCV/UdemyCourse/Section4/BluringAndSmoothing.py
+++ OpenCV/UdemyCourse/Section4/BluringAndSmoothing.py
@@ -5,9 +5,24 @@ import matplotlib.pyplot as plt
 def show_pic(img):
     fig = plt.figure(figsize=(15,15))
     ax = fig.add_subplot(111)
-    ax.imshow(img,cv2.COLOR_BGR2RGB)
+    ax.imshow(img)
     plt.show()
 
-img = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\bricks.jpg').astype(np.float32) / 255
+def load_img():
+    img = cv2.imread(r'C:\Users\User\Downloads\Computer-Vision-with-Python\DATA\bricks.jpg').astype(np.float32) / 255
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
 
-show_pic(img)
+
+img = load_img()
+
+gamma = 1/4
+result = np.power(img, gamma)
+
+font = cv2.FONT_HERSHEY_COMPLEX
+cv2.putText(img,text='bricks',org=(10,600),fontFace=font,fontScale=10,color=(255,0,0),thickness=4)
+
+kernel = np.ones(shape=(5,5),dtype=np.float32)/25
+print(kernel)
+
+#show_pic(img)
